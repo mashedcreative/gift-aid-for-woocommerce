180f386aa70860a6be69f5137bd1d1d070a2c76a
Dave Green <hello@davetgreen.me>
2015-09-10 10:31:46 +0100

    Updated plugin name

--- woocommerce-gift-aid/woocommerce-gift-aid.php
+++ woocommerce-gift-aid/woocommerce-gift-aid.php
@@ -13,7 +13,7 @@
  * @package           WooCommerce_Gift_Aid
  *
  * @wordpress-plugin
- * Plugin Name:       WordPress Plugin Boilerplate
+ * Plugin Name:       WooCommerce Gift Aid
  * Plugin URI:        https://github.com/mkdo/woocommerce-gift-aid/
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
