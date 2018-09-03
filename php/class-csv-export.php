772be0d7777956f59069bf297692ca77503d0c5c
KT <kt@mashedcreative.com>
2018-09-03 13:04:03 +0100

    Made compatible with the "latest as of now" WooCommerce Customer/Order CSV Export Version 4.5.2

--- php/class-csv-export.php
+++ php/class-csv-export.php
@@ -109,9 +109,16 @@ class CSV_Export {
 
 		// Merge our data with the existing row data.
 		$new_order_data = array();
+		$one_row_per_item = false;
 
-		if ( isset( $csv_generator->order_format ) && ( 'default_one_row_per_item' == $csv_generator->order_format || 'legacy_one_row_per_item' == $csv_generator->order_format ) ) {
-
+		if ( version_compare( wc_customer_order_csv_export()->get_version(), '4.0.0', '<' ) ) {
+			// pre 4.0 compatibility
+			$one_row_per_item = ( 'default_one_row_per_item' === $csv_generator->order_format || 'legacy_one_row_per_item' === $csv_generator->order_format );
+		} elseif ( isset( $csv_generator->format_definition ) ) {
+			// post 4.0 (requires 4.0.3+)
+			$one_row_per_item = 'item' === $csv_generator->format_definition['row_type'];
+		}
+		if ( $one_row_per_item ) {
 			foreach ( $order_data as $data ) {
 				$new_order_data[] = array_merge( (array) $data, $custom_data );
 			}
